b6ff5254df245c1c29311ed6a26d25024abe21d2
Louise Reid <l_reid@hotmail.co.uk>
2019-03-10 16:28:57 +0000

    Updates

---
+++ precache-manifest.9d3ab6c2aac6311e485bbae0c17472fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d5bdb605f3fc7e2274f",
+    "url": "/LouiseReid/React-Hooks-Bucket-List/static/js/runtime~main.30b0dc6c.js"
+  },
+  {
+    "revision": "d19d53ce8b02ef903b1b",
+    "url": "/LouiseReid/React-Hooks-Bucket-List/static/js/main.74351d85.chunk.js"
+  },
+  {
+    "revision": "1a18ff132b87c5598bb3",
+    "url": "/LouiseReid/React-Hooks-Bucket-List/static/js/2.68632914.chunk.js"
+  },
+  {
+    "revision": "d19d53ce8b02ef903b1b",
+    "url": "/LouiseReid/React-Hooks-Bucket-List/static/css/main.9db45235.chunk.css"
+  },
+  {
+    "revision": "c312510924b324b8887e6c49f063a09a",
+    "url": "/LouiseReid/React-Hooks-Bucket-List/index.html"
+  }
+];
