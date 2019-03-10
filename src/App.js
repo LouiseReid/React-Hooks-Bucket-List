aa20923400d795079daae429701cbb5f5ecafdb6
Louise Reid <l_reid@hotmail.co.uk>
2019-03-10 12:05:19 +0000

    skeleton set up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, useReducer} from 'react';
+import BucketListContext from './store/context';
+import bucketListReducer from './store/reducer';
+import CountriesContainer from './contianers/CountriesContainer';
+import BucketList from './contianers/BucketList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App(){
+  const initialState = useContext(BucketListContext)
+  const [state, dispatch] = useReducer(bucketListReducer, initialState)
+
+  return (
+    <BucketListContext.Provider value={{state, dispatch}}>
+      <CountriesContainer />
+      <BucketList />
+    </BucketListContext.Provider>
+  )
 }
-
-export default App;
